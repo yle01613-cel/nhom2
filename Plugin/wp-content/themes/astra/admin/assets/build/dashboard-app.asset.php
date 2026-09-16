9b36216a080546f227df781ca411f4aaaddbc0e5
yle01613-cel <yle01613@gmail.com>
2026-09-16 09:57:03 +0700

    Delete Plugin directory

--- Plugin/wp-content/themes/astra/admin/assets/build/dashboard-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '3f306bc2d64cb1113635');
--- Plugin/wp-content/themes/astra/admin/assets/theme-builder/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-i18n'), 'version' => '3f1beedfd91805f4c11c');
